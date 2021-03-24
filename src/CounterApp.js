af176994d2f8a579ab6e81056963dc0f9d44d477
memof90 <memof80@gmail.com>
2021-03-24 08:23:08 -0500

    hooks useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({value}) => {
+const CounterApp = ({value = 0}) => {
+
+    // Hooks
+    const [counter, setCounter] = useState(value);
+    
 
     const hangleAdd = () => {
-       console.log('hello');
+     setCounter(counter + 1);
+    //  setCounter((c) => c + 1);
+    }
+    const handledSubstrac = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset  = () => {
+        // setCounter((c) => c = 0);
+        setCounter(value);
     }
 
     return(
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
 
             <button onClick={ hangleAdd }>+ 1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handledSubstrac}>- 1</button>
         </>
     )
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
+    value: PropTypes.number
 }
 
 export default CounterApp;
