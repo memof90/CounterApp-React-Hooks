166ad693d989844461d4fc765683b5e6bb495c97
memof90 <memof80@gmail.com>
2021-03-24 07:46:04 -0500

    Evento clikc

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+
+    const hangleAdd = () => {
+       console.log('hello');
+    }
+
     return(
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+
+            <button onClick={ hangleAdd }>+ 1</button>
         </>
     )
 }
